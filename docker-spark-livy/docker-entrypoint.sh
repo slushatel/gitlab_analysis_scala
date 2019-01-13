9758fe48b1a7e842911b6fad9c092fd7150f889e
stas <q>
2019-01-13 22:24:58 +0200

    docker spark livy

---
+++ docker-spark-livy/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+cd $SPARK_HOME
+bin/spark-class org.apache.spark.deploy.master.Master -h master &
+sleep 10
+/usr/livy/livy-0.4.0-incubating-bin/bin/livy-server start &
+
+
+cleanup ()                                                                 
+{                                                                          
+  kill -s SIGTERM $!                                                         
+  exit 0                                                                     
+}                                                                          
+                                                                           
+trap cleanup SIGINT SIGTERM                                                
+                                                                           
+while [ 1 ]                                                                
+do                                                                         
+  sleep 60 &                                                             
+  wait $!                                                                
+done
